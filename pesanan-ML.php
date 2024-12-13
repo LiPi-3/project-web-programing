97b9b2cf3c737ff47d46497b7ac731dcf3bd4ca2
Akita162 <cyan.gamers162@gmail.com>
2024-12-13 19:38:59 +0700

    fix filter menu pesanan ML

--- pesanan-ML.php
+++ pesanan-ML.php
@@ -20,11 +20,12 @@ $produk = [
   ['18', '', '18', '18', ' weekly Diamond Pass (+ Bonus) | Rp', 'weekly'],
   ['19', '', '19', '19', ' weekly Diamond Pass (+ Bonus) | Rp', 'weekly'],
   ['2', '', '2', '2', ' weekly Diamond Pass (+ Bonus) | Rp', 'weekly'],
-  ]
+]
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -37,40 +38,38 @@ $produk = [
   <link rel="stylesheet" href="styles.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
+
 <body>
   <header>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="home.php">Lawak Store</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <!--toogle icon-->
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="home.php"
-                >Beranda</a>
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              ></a>
-              <div class="Login-link">
-                <a class="nav-link" href="registrasi.php">Daftar/Masuk</a>
-              </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="home.php">Lawak Store</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <!--toogle icon-->
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="home.php">Beranda</a>
+            <a
+              class="nav-link disabled"
+              href="#"
+              tabindex="-1"
+              aria-disabled="true"></a>
+            <div class="Login-link">
+              <a class="nav-link" href="registrasi.php">Daftar/Masuk</a>
             </div>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   </header>
 
   <main class="container my-4">
@@ -82,93 +81,93 @@ $produk = [
     </section>
     <br>
     <section>
-    <h2 class="h5">Pilih Nominal Top Up</h2>
-        <div class="container-fluid justify-content-around" id="ItemFilter">
+      <h2 class="h5">Pilih Nominal Top Up</h2>
+      <div class="container-fluid justify-content-around" id="ItemFilter">
         <button type="button" class="btn btn-outline-primary" id="diamond" onclick="filterProduk(this)">Diamonds</button>
         <button type="button" class="btn btn-outline-primary" id="weekly" onclick="filterProduk(this)">Weakly Diamond Pass</button>
         <button type="button" class="btn btn-outline-primary" id="twilight" onclick="filterProduk(this)">Twilight Pass</button>
-        </div>
+      </div>
     </section>
     <br>
     <!-- Pilih Nominal Top Up -->
     <form action="process.php" method="POST" class="row g-3">
-    <section class="my-4">
+      <section class="my-4">
 
-      <div class="row g-3">
-      <?php foreach ($produk as $i) { ?>
-        <div class="col-md-3">
-        <input type="radio" class="btn-check filter-item <?= $i[5] ?>" name="options" id="option<?= $i[0] ?>" value="<?= $i[1] ?>" autocomplete="off">
-        <label class="btn btn-outline-primary w-100 h-100" name="pilihan<?= $i[2] ?>" for="option<?= $i[3] ?>"><?= $i[4] ?></label>
-        </div>
-      <?php } ?>
-        <!-- Tambahkan item sesuai kebutuhan -->
-      </div>
-
-    </section>
-
-    <!-- Form User ID -->
-    <section class="my-4">
-    <div class="row g-3">
-      <h2 class="h5">Masukkan Player ID</h2>
-        <div class="col-md-6">
-          <label for="user_id" class="form-label">Masukan Player ID</label>
-          <input type="text" id="user_id" name="user_id" class="form-control" required>
+        <div class="row g-3">
+          <?php foreach ($produk as $i) { ?>
+            <div class="col-md-3 filter-item <?= $i[5] ?>">
+              <input type="radio" class="btn-check" name="options" id="option<?= $i[0] ?>" value="<?= $i[1] ?>" autocomplete="off">
+              <label class="btn btn-outline-primary w-100 h-100" name="pilihan<?= $i[2] ?>" for="option<?= $i[3] ?>"><?= $i[4] ?></label>
+            </div>
+          <?php } ?>
+          <!-- Tambahkan item sesuai kebutuhan -->
         </div>
 
-        <div class="col-md-6">
-          <label for="whatsapp" class="form-label">Nomer Whatsapp</label>
-          <input type="text" onkeypress="return restrictAlphabets(event)" id="whatsapp" name="whatsapp" class="form-control" required>
-        </div>
+      </section>
 
-    </section>
+      <!-- Form User ID -->
+      <section class="my-4">
+        <div class="row g-3">
+          <h2 class="h5">Masukkan Player ID</h2>
+          <div class="col-md-6">
+            <label for="user_id" class="form-label">Masukan Player ID</label>
+            <input type="text" id="user_id" name="user_id" class="form-control" required>
+          </div>
 
-    <!-- Metode Pembayaran -->
-    <section class="my-4">
-      <h2 class="h5">Pilih Metode Pembayaran</h2>
-      <div class="row g-3">
-      <div class="col-md-4">
-          <select class="form-select" id="server" aria-label="Default select example">
-          <option value="bank">Silakan Pilih</option>
-          <option value="bca">Bank Transfer | BCA</option>
-          <option value="bri">Bank Transfer | BRI</option>
-          <option value="bni">Bank Transfer | BNI</option>
-          <option value="dana">Digital Wallet | DANA</option>
-          <option value="gopay">Digital Wallet | GoPay</option>
-          <option value="qris">QRIS</option>
-          </select>
+          <div class="col-md-6">
+            <label for="whatsapp" class="form-label">Nomer Whatsapp</label>
+            <input type="text" onkeypress="return restrictAlphabets(event)" id="whatsapp" name="whatsapp" class="form-control" required>
+          </div>
+
+      </section>
+
+      <!-- Metode Pembayaran -->
+      <section class="my-4">
+        <h2 class="h5">Pilih Metode Pembayaran</h2>
+        <div class="row g-3">
+          <div class="col-md-4">
+            <select class="form-select" id="server" aria-label="Default select example">
+              <option value="bank">Silakan Pilih</option>
+              <option value="bca">Bank Transfer | BCA</option>
+              <option value="bri">Bank Transfer | BRI</option>
+              <option value="bni">Bank Transfer | BNI</option>
+              <option value="dana">Digital Wallet | DANA</option>
+              <option value="gopay">Digital Wallet | GoPay</option>
+              <option value="qris">QRIS</option>
+            </select>
+          </div>
         </div>
-      </div>
-    </section>
-    <section class="my-4">
+      </section>
+      <section class="my-4">
         <h2 class="h5"></h2>
         <div class="row g-3">
-        <div class="col-md-4">
-        <button type="submit" class="btn btn-success">Konfirmasi Pesanan</button>
+          <div class="col-md-4">
+            <button type="submit" class="btn btn-success">Konfirmasi Pesanan</button>
+          </div>
         </div>
-        </div>
-    </section>
+      </section>
     </form>
   </main>
 
   <footer class="bg-primary text-center text-lg-start">
-        <div class="text-center p-3 text-light">
-        © 2024 Copyright Lawak Store
-        </div>
+    <div class="text-center p-3 text-light">
+      © 2024 Copyright Lawak Store
+    </div>
   </footer>
   <script>
     function filterProduk(e) {
       console.log(e.id);
       $('.filter-item').removeClass('d-none')
-      if (e.id != "All") $('a.filter-item:not(.' + e.id + ')').addClass('d-none');
+      if (e.id != "All") $('.filter-item:not(.' + e.id + ')').addClass('d-none');
       $('#ItemFilter button').removeClass('active');
       e.classList.add('active')
     }
   </script>
 
   <script type="text/javascript">
-    function restrictAlphabets(e){
+    function restrictAlphabets(e) {
       var x = e.which || e.keycode;
-      if((x >= 48 && x <= 57))
+      if ((x >= 48 && x <= 57))
         return true;
       else
         return false;
@@ -176,4 +175,5 @@ $produk = [
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
